36dc0a2fd8321dcb3f074bb7646fb91adefc2e2f
Israel Mesa <mesa.israel50@gmail.com>
2017-07-15 14:50:38 +0000

    v1

---
+++ todos.js
@@ -0,0 +1,16 @@
+["item 1", "item 2", "item 3"]
+
+var todos = ["item 1", "item 2", "item 3"] // 1.) place to store todos
+
+console.log('My Todos:', todos) // 2.) place to print todos
+
+todos.push("item 4") // 3.) way to add more todos
+
+todos[0] = "item 1 updated" // 4.) way to change an existing todos
+
+todos.splice(1, 2) //.5) way to delete a todos 
+
+console.log(todos) 
+
+
+
