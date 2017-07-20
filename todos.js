ae0ba8b5f3b1aaaa09a511400bdc18e94f017d00
Israel Mesa <mesa.israel50@gmail.com>
2017-07-20 01:35:04 +0000

    v5

--- todos.js
+++ todos.js
@@ -1,24 +1,72 @@
-//Version 4
+// Version 5 - For Loops
+
+//i = 0 - Initialization
+//Say "Hey" IF i < 3 - Condition
+//Increase i by 1 - Final-Expression
+
+//0 "Hey"
+//1 "Hey"
+//2 "Hey"
+//3 i is no longer < 3
+
+// for(Initialization, Condition, Final-Expression) {
+// console.log("Hey");
+//}
+
+// for (var i = 0; i < 3; i++) {
+    // console.log("Hey");
+// }
+ 
+// for (var i = 0; i < 3; i++) {
+    // console.log(i);
+// }
+
+// var testArray = ['item 1', 'item 2', 'item 3'];
+
+// for (var i = 0; i < testArray.length; i++) {
+    // console.log(i);
+    // console.log(testArray[i]);
+// }
+
+// testArray.push('extra item')
+
+//for (var i = 0; i < testArray.length; i++) {
+    // console.log(i);
+    // console.log(testArray[i]);
+// }
 
 var todoList = {
-    todos: [], // todos array ( which contains an object) is a property in an object
+    todos: [], 
     
-    displayTodos: function ( ){             //Method to display todos
-        console.log('My Todos:', this.todos);
+    displayTodos: function ( ){     //displays todoText within each Object added in the array todos         
+        if (this.todos.length === 0) {
+            console.log('your todo list is empty!')
+        }
+        else {
+        console.log('My Todos:');
+        for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+            console.log ('(x)', this.todos[i].todoText);
+            }
+        else {
+            console.log ('()', this.todos[i].todoText);
+            }
+        }
+        }
     },
-    addTodo: function (todoText) {   //Method to add todos    
+    addTodo: function (todoText) {       
     this.todos.push({
         todoText: todoText,
-        completed: false // Boolean true or false
+        completed: false 
     });
     this.displayTodos( );
     },
-    changeTodo: function (position, todoText){    //Method to change todos
+    changeTodo: function (position, todoText){    
     this.todos[position].todoText = todoText;
-    // this.todos[position] = newValue;
+    
     this.displayTodos( );
     },
-    deleteTodo: function (position, numItems){    //Method to delete todos
+    deleteTodo: function (position, numItems){    
     this.todos.splice(position, numItems);
     this.displayTodos( );
     },
@@ -30,18 +78,9 @@ var todoList = {
     }
 };
 
-//todoList.displayTodos( )
-
-//todoList.addTodo('this is an Object')
-
-//todoList.changeTodo(0, 'second try')
-
-todoList.addTodo('boolean testing')
+todoList.addTodo('code!')
 
 todoList.toggleCompleted(0)
-
-
-
 
 
 
